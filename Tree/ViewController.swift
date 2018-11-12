4a1efded11ee3b479f3c46f727ec1102acdc3dcb
周袁德 <zhouyuande@zhouyuandes-mac.local>
2018-11-12 15:37:17 +0800

    Initial Commit

---
+++ Tree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tree
+//
+//  Created by 周袁德 on 2018/11/12.
+//  Copyright © 2018年 周袁德. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TreeTests/TreeTests.swift
@@ -0,0 +1,34 @@
+//
+//  TreeTests.swift
+//  TreeTests
+//
+//  Created by 周袁德 on 2018/11/12.
+//  Copyright © 2018年 周袁德. All rights reserved.
+//
+
+import XCTest
+@testable import Tree
+
+class TreeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
